554d8053fd3c20d9dcd86bf8a060810bc2bfffad
Masood Aweaz <aweaz7@gmail.com>
2021-10-23 10:56:26 +0530

    added draw_cylinder shape

--- main.cpp
+++ main.cpp
@@ -330,6 +330,48 @@ void setMaterial(GLfloat ambientR, GLfloat ambientG, GLfloat ambientB,
 
 }
 
+void draw_cylinder(GLfloat radius,
+                   GLfloat height,
+                   GLubyte R,
+                   GLubyte G,
+                   GLubyte B)
+{
+    GLfloat x              = 0.0;
+    GLfloat y              = 0.0;
+    GLfloat angle          = 0.0;
+    GLfloat angle_stepsize = 0.1;
+
+    //Draw the tube
+    //glColor3ub(R-40,G-40,B-40);
+    glColor3f(1, 0.7, 0);
+    glBegin(GL_QUAD_STRIP);
+    angle = 0.0;
+        while( angle < 2*PI ) {
+            x = 2*radius * cos(angle);
+            y = 2*radius * sin(angle);
+            glVertex3f(x, y , height);
+            glVertex3f(x, y , 0.0);
+            angle = angle + angle_stepsize;
+        }
+        glVertex3f(radius, 0.0, height);
+        glVertex3f(radius, 0.0, 0.0);
+    glEnd();
+
+    //Draw the circle on top of cylinder
+    //glColor3ub(R,G,B);
+    glColor3f(1, 0, 0.2);
+    glBegin(GL_POLYGON);
+    angle = 0.0;
+        while( angle < 2*PI ) {
+            x = 2*radius * cos(angle);
+            y = 2*radius * sin(angle);
+            glVertex3f(x, y , height);
+            angle = angle + angle_stepsize;
+        }
+        glVertex3f(radius, 0.0, height);
+    glEnd();
+}
+
 void coloredCube(){
     
     glColor3f((GLfloat)184/255, (GLfloat)115/255, (GLfloat)51/255);
